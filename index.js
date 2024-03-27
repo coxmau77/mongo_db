9c53941eadf190bca38dc47f8025740d70d58050
Mauricio Cox <maucox77@gmail.com>
2024-03-26 21:11:13 -0300

    chore: creacion de servidor con express

--- index.js
+++ index.js
@@ -11,3 +11,4 @@ app.listen(port, () => {
     console.log(`Puerto activo en http://localhost:${port}`)
 });
 // https://youtu.be/xkHyM-K3Cd8?list=PLPl81lqbj-4Iy7yuRrVLn4V6isOVpvlpl
+// https://bluuweb.github.io/desarrollo-web-bluuweb/20-01-node/#servidor-http
